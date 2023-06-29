82a9b47a74909c9e4bf8d8ebf3c6e352edb43d17
hanfei <hanfei@hanfeideMacBook-Pro.local>
2023-06-29 10:52:30 +0800

    feat: simplify repo

--- src/lib/vue-vite.spec.ts
+++
@@ -1,7 +0,0 @@
-import { vueVite } from './vue-vite';
-
-describe('vueVite', () => {
-  it('should work', () => {
-    expect(vueVite()).toEqual('vue-vite');
-  });
-});
--- src/lib/vue-vite.ts
+++
@@ -1,3 +0,0 @@
-export function vueVite(): string {
-  return 'vue-vite';
-}
