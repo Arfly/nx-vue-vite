ac8b8131cc2f891cf1c1d97249c6093f60ca7ac0
hanfei <hanfei@hanfeideMacBook-Pro.local>
2023-06-30 16:32:39 +0800

    feat: demo

---
+++ libs/my-plugin/src/generators/vue/generator.ts
@@ -0,0 +1,73 @@
+import {
+  addDependenciesToPackageJson,
+  addProjectConfiguration,
+  formatFiles,
+  generateFiles,
+  GeneratorCallback,
+  runTasksInSerial,
+  Tree,
+} from '@nx/devkit';
+import * as path from 'path';
+import { VueGeneratorSchema } from './schema';
+
+const DEPENDENCY = {
+  dependencies: {
+    vue: '^3.2.47',
+  },
+  devDependencies: {
+    '@vitejs/plugin-vue': '^4.1.0',
+    typescript: '^5.0.2',
+    vite: '^4.3.9',
+    'vue-tsc': '^1.4.2',
+  },
+};
+
+export async function vueGenerator(tree: Tree, options: VueGeneratorSchema) {
+  const projectRoot = `apps/${options.name}`;
+  addProjectConfiguration(tree, options.name, {
+    root: projectRoot,
+    projectType: 'application',
+    sourceRoot: `${projectRoot}/src`,
+    targets: {
+      dev: {
+        inputs: [
+          {
+            externalDependencies: ['vite'],
+          },
+        ],
+        command: `cd ${projectRoot} && vite`,
+        dependsOn: ['^build'],
+      },
+      build: {
+        inputs: [
+          {
+            externalDependencies: ['vite', 'vue-tsc'],
+          },
+        ],
+        command: `cd ${projectRoot} && vue-tsc && vite build`,
+        dependsOn: ['^build'],
+      },
+      preview: {
+        inputs: [
+          {
+            externalDependencies: ['vite'],
+          },
+        ],
+        command: `cd ${projectRoot} && vite preview`,
+      },
+    },
+  });
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  await formatFiles(tree);
+  const tasks: GeneratorCallback[] = [];
+  const installTask = await addDependenciesToPackageJson(
+    tree,
+    DEPENDENCY.dependencies,
+    DEPENDENCY.devDependencies
+  );
+  tasks.push(installTask);
+
+  return runTasksInSerial(...tasks);
+}
+
+export default vueGenerator;
--- src/generators/comp/schema.d.ts
+++
@@ -1,3 +0,0 @@
-export interface CompGeneratorSchema {
-  name: string;
-}
